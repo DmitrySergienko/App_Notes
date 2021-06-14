d99eed0c7501b83aaf42a7a18be86e4588ff7af1
DmitrySergienko <dmitrisergienko@gmail.com>
2021-06-14 11:53:12 +0300

    Buttons for fragments created

--- app/src/main/java/ru/ds/app_notes/MainActivity.java
+++ app/src/main/java/ru/ds/app_notes/MainActivity.java
@@ -3,6 +3,7 @@ package ru.ds.app_notes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClickMain(View view) {
+    }
+
+    public void onClickPhoto(View view) {
+    }
+
+    public void onClickDescr(View view) {
+    }
+
+    public void onClickSettings(View view) {
     }
 }
